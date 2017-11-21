6a36e6462ed65feed1c30b23ba8bf5289f4aa110
Richard Lavey <richard.lavey@calastone.com>
2017-11-21 09:05:19 +0000

    quote variable
    Signed-off-by: Richard Lavey <richard.lavey@calastone.com>

--- lib/kitchen/driver/ec2.rb
+++ lib/kitchen/driver/ec2.rb
@@ -565,7 +565,7 @@ module Kitchen
         # Returning the fully constructed PowerShell script to user_data
         Kitchen::Util.outdent!(<<-EOH)
         <powershell>
-        $logfile=#{logfile_name}
+        $logfile="#{logfile_name}"
         # EC2Launch doesn't init extra disks by default
         #{disk_init}
         # Allow script execution
